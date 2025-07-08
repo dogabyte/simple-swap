b09dcdee5f3389e2f0203a0fb67c8f1b3f6a1bac
Gabriel <donatogabriel@outlook.com>
2025-07-08 17:19:38 -0300

    Update contract

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SimpleSwap is ERC20 {
